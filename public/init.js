c6beba27147817cf801f4a72423f383a12a790fe
wangxiongfei <wangxiongfei@cmii.chinamobile.com>
2023-01-06 14:49:08 +0800

    feat: 简化demo、添加.vscode/settings.josn文件

--- public/init.js
+++ public/init.js
@@ -3,43 +3,43 @@
 import FilerobotImageEditor from '../packages/filerobot-image-editor/src';
 import config from './demo-config';
 
-function getElementById(id) {
-  return document.getElementById(id);
-}
+// function getElementById(id) {
+//   return document.getElementById(id);
+// }
 
-const crop = getElementById('crop');
-const finetune = getElementById('finetune');
-const filter = getElementById('filter');
-const filterLabel = getElementById('filter-label');
-const watermark = getElementById('watermark');
-const annotate = getElementById('annotate');
-const annotateLabel = getElementById('annotate-label');
-const resize = getElementById('resize');
-const addImg = getElementById('add-image');
-const modeOptions = getElementById('mode-options');
-const jsTabTitle = getElementById('js-code-tab');
-const jsCodeWrapper = getElementById('js-code-wrapper');
-const reactTabTitle = getElementById('react-code-tab');
-const reactCodeWrapper = getElementById('react-code-wrapper');
-const cdnTabTitle = getElementById('cdn-code-tab');
-const cdnCodeWrapper = getElementById('cdn-code-wrapper');
-const copyButtons = document.querySelectorAll('.copy-button');
-const accordions = document.querySelectorAll('[data-accordion]');
+// const crop = getElementById('crop');
+// const finetune = getElementById('finetune');
+// const filter = getElementById('filter');
+// const filterLabel = getElementById('filter-label');
+// const watermark = getElementById('watermark');
+// const annotate = getElementById('annotate');
+// const annotateLabel = getElementById('annotate-label');
+// const resize = getElementById('resize');
+// const addImg = getElementById('add-image');
+// const modeOptions = getElementById('mode-options');
+// const jsTabTitle = getElementById('js-code-tab');
+// const jsCodeWrapper = getElementById('js-code-wrapper');
+// const reactTabTitle = getElementById('react-code-tab');
+// const reactCodeWrapper = getElementById('react-code-wrapper');
+// const cdnTabTitle = getElementById('cdn-code-tab');
+// const cdnCodeWrapper = getElementById('cdn-code-wrapper');
+// const copyButtons = document.querySelectorAll('.copy-button');
+// const accordions = document.querySelectorAll('[data-accordion]');
 
-let useCloudimage = false;
+const useCloudimage = false;
 const { TABS } = FilerobotImageEditor;
 
-const EXAMPLE_CODE_TABS = {
-  'js-code-tab': jsCodeWrapper,
-  'react-code-tab': reactCodeWrapper,
-  'cdn-code-tab': cdnCodeWrapper,
-};
+// const EXAMPLE_CODE_TABS = {
+//   'js-code-tab': jsCodeWrapper,
+//   'react-code-tab': reactCodeWrapper,
+//   'cdn-code-tab': cdnCodeWrapper,
+// };
 
-const DEFAULT_IMAGES_SRCS = [
-  'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/cnxLfVasJJA3VmpYi5d6P9.jpg',
-  'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/DwuZYKT82Ly3F3a4pP6YxR.jpg',
-  'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/KRVmdSsRi6iJHf7NsVgVAJ.jpg',
-];
+// const DEFAULT_IMAGES_SRCS = [
+//   'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/cnxLfVasJJA3VmpYi5d6P9.jpg',
+//   'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/DwuZYKT82Ly3F3a4pP6YxR.jpg',
+//   'https://images-1307121509.cos.ap-beijing.myqcloud.com/souJpg/images/KRVmdSsRi6iJHf7NsVgVAJ.jpg',
+// ];
 
 const selectedTabs = [
   TABS.ADJUST,
@@ -50,14 +50,14 @@ const selectedTabs = [
   TABS.RESIZE,
 ];
 
-const IMG_EDITOR_TABS = {
-  adjust: TABS.ADJUST,
-  finetune: TABS.FINETUNE,
-  filter: TABS.FILTERS,
-  watermark: TABS.WATERMARK,
-  annotate: TABS.ANNOTATE,
-  resize: TABS.RESIZE,
-};
+// const IMG_EDITOR_TABS = {
+//   adjust: TABS.ADJUST,
+//   finetune: TABS.FINETUNE,
+//   filter: TABS.FILTERS,
+//   watermark: TABS.WATERMARK,
+//   annotate: TABS.ANNOTATE,
+//   resize: TABS.RESIZE,
+// };
 
 const pluginConfig = {
   ...config,
@@ -111,179 +111,179 @@ filerobotImageEditor.render({
   useCloudimage,
 });
 
-function onChangeTabsHandler(event) {
-  const { target } = event;
-  const { value, checked } = target;
-  const nextTab = IMG_EDITOR_TABS[value];
+// function onChangeTabsHandler(event) {
+//   const { target } = event;
+//   const { value, checked } = target;
+//   const nextTab = IMG_EDITOR_TABS[value];
 
-  if (checked) {
-    if (!selectedTabs.includes(nextTab)) {
-      selectedTabs.push(nextTab);
-    }
-  } else {
-    const removedTabIndex = selectedTabs.indexOf(nextTab);
+//   if (checked) {
+//     if (!selectedTabs.includes(nextTab)) {
+//       selectedTabs.push(nextTab);
+//     }
+//   } else {
+//     const removedTabIndex = selectedTabs.indexOf(nextTab);
 
-    if (selectedTabs.includes(nextTab) && selectedTabs.length === 1) {
-      target.checked = true;
-      return;
-    }
+//     if (selectedTabs.includes(nextTab) && selectedTabs.length === 1) {
+//       target.checked = true;
+//       return;
+//     }
 
-    selectedTabs.splice(removedTabIndex, 1);
-  }
+//     selectedTabs.splice(removedTabIndex, 1);
+//   }
 
-  filerobotImageEditor.render({ tabsIds: [...selectedTabs] });
-}
+//   filerobotImageEditor.render({ tabsIds: [...selectedTabs] });
+// }
 
-function toggleActiveImage(imageContainer, imageSrc) {
-  const removeResizeParamRegex = /\?.+/g;
-  const imageUrl = imageSrc.replace(removeResizeParamRegex, '');
-  const prevImageContainer = document.querySelector(
-    '[data-image-editor-active-image]',
-  );
+// function toggleActiveImage(imageContainer, imageSrc) {
+//   const removeResizeParamRegex = /\?.+/g;
+//   const imageUrl = imageSrc.replace(removeResizeParamRegex, '');
+//   const prevImageContainer = document.querySelector(
+//     '[data-image-editor-active-image]',
+//   );
 
-  if (prevImageContainer) {
-    prevImageContainer.removeAttribute('data-image-editor-active-image');
-  }
+//   if (prevImageContainer) {
+//     prevImageContainer.removeAttribute('data-image-editor-active-image');
+//   }
 
-  imageContainer.setAttribute('data-image-editor-active-image', '');
+//   imageContainer.setAttribute('data-image-editor-active-image', '');
 
-  filerobotImageEditor.render({ source: imageUrl });
-}
+//   filerobotImageEditor.render({ source: imageUrl });
+// }
 
-function appendImageToContainer(imageSrc) {
-  const imagesWrapper = document.querySelector('.uploaded-imgs-wrapper');
-  const imageWrapper = document.createElement('div');
+// function appendImageToContainer(imageSrc) {
+//   const imagesWrapper = document.querySelector('.uploaded-imgs-wrapper');
+//   const imageWrapper = document.createElement('div');
 
-  imageWrapper.style.backgroundImage = `url(${imageSrc})`;
+//   imageWrapper.style.backgroundImage = `url(${imageSrc})`;
 
-  imageWrapper.className = 'uploaded-img';
+//   imageWrapper.className = 'uploaded-img';
 
-  imageWrapper.onclick = () => toggleActiveImage(imageWrapper, imageSrc);
+//   imageWrapper.onclick = () => toggleActiveImage(imageWrapper, imageSrc);
 
-  imagesWrapper.appendChild(imageWrapper);
-  imagesWrapper.scrollTop = imagesWrapper.scrollHeight;
+//   imagesWrapper.appendChild(imageWrapper);
+//   imagesWrapper.scrollTop = imagesWrapper.scrollHeight;
 
-  return imageWrapper;
-}
+//   return imageWrapper;
+// }
 
-function uploadImg(event) {
-  const imageSrc = URL.createObjectURL(event.target.files[0]);
+// function uploadImg(event) {
+//   const imageSrc = URL.createObjectURL(event.target.files[0]);
 
-  const imageContainer = appendImageToContainer(imageSrc);
+//   const imageContainer = appendImageToContainer(imageSrc);
 
-  toggleActiveImage(imageContainer, imageSrc);
+//   toggleActiveImage(imageContainer, imageSrc);
 
-  filerobotImageEditor.render({ source: imageSrc });
-}
+//   filerobotImageEditor.render({ source: imageSrc });
+// }
 
-function changeModeHandler() {
-  if (modeOptions.value === 'Cloudimage') {
-    annotate.checked = false;
-    annotate.disabled = true;
-    annotateLabel.style.color = 'gray';
-    annotateLabel.style.cursor = 'auto';
+// function changeModeHandler() {
+//   if (modeOptions.value === 'Cloudimage') {
+//     annotate.checked = false;
+//     annotate.disabled = true;
+//     annotateLabel.style.color = 'gray';
+//     annotateLabel.style.cursor = 'auto';
 
-    filter.checked = false;
-    filter.disabled = true;
-    filterLabel.style.color = 'gray';
-    filterLabel.style.cursor = 'auto';
+//     filter.checked = false;
+//     filter.disabled = true;
+//     filterLabel.style.color = 'gray';
+//     filterLabel.style.cursor = 'auto';
 
-    useCloudimage = true;
-  } else {
-    if (selectedTabs.includes(annotate.name)) {
-      annotate.checked = true;
-    }
+//     useCloudimage = true;
+//   } else {
+//     if (selectedTabs.includes(annotate.name)) {
+//       annotate.checked = true;
+//     }
 
-    if (selectedTabs.includes(filter.name)) {
-      filter.checked = true;
-    }
+//     if (selectedTabs.includes(filter.name)) {
+//       filter.checked = true;
+//     }
 
-    filter.disabled = false;
-    filterLabel.style.color = '#203254';
-    filterLabel.style.cursor = 'pointer';
-    annotate.disabled = false;
-    annotateLabel.style.color = '#203254';
-    annotateLabel.style.cursor = 'pointer';
+//     filter.disabled = false;
+//     filterLabel.style.color = '#203254';
+//     filterLabel.style.cursor = 'pointer';
+//     annotate.disabled = false;
+//     annotateLabel.style.color = '#203254';
+//     annotateLabel.style.cursor = 'pointer';
 
-    useCloudimage = false;
-  }
+//     useCloudimage = false;
+//   }
 
-  filerobotImageEditor.render({ useCloudimage, tabsIds: [...selectedTabs] });
-}
+//   filerobotImageEditor.render({ useCloudimage, tabsIds: [...selectedTabs] });
+// }
 
-function changeCodeTabHandler(event) {
-  const selectedCodeTabId = event.target.id;
-  const selectedCode = EXAMPLE_CODE_TABS[selectedCodeTabId];
+// function changeCodeTabHandler(event) {
+//   const selectedCodeTabId = event.target.id;
+//   const selectedCode = EXAMPLE_CODE_TABS[selectedCodeTabId];
 
-  Object.values(EXAMPLE_CODE_TABS).forEach((codeTab) => {
-    // eslint-disable-next-line no-param-reassign
-    codeTab.style.display = 'none';
-  });
+//   Object.values(EXAMPLE_CODE_TABS).forEach((codeTab) => {
+//     // eslint-disable-next-line no-param-reassign
+//     codeTab.style.display = 'none';
+//   });
 
-  selectedCode.style.display = 'unset';
-}
+//   selectedCode.style.display = 'unset';
+// }
 
-function toggleActiveCodeTab(event) {
-  const nextCodeTab = event.target || event;
+// function toggleActiveCodeTab(event) {
+//   const nextCodeTab = event.target || event;
 
-  changeCodeTabHandler(event);
+//   changeCodeTabHandler(event);
 
-  const prevCodeTab = document.querySelector('[selected-tab]');
+//   const prevCodeTab = document.querySelector('[selected-tab]');
 
-  if (prevCodeTab) {
-    prevCodeTab.removeAttribute('selected-tab');
-  }
+//   if (prevCodeTab) {
+//     prevCodeTab.removeAttribute('selected-tab');
+//   }
 
-  nextCodeTab.setAttribute('selected-tab', '');
-}
+//   nextCodeTab.setAttribute('selected-tab', '');
+// }
 
-document.onreadystatechange = () => {
-  DEFAULT_IMAGES_SRCS.forEach((imageSrc, index) => {
-    const imageContainer = appendImageToContainer(imageSrc);
+// document.onreadystatechange = () => {
+//   DEFAULT_IMAGES_SRCS.forEach((imageSrc, index) => {
+//     const imageContainer = appendImageToContainer(imageSrc);
 
-    if (!index) {
-      toggleActiveImage(imageContainer, imageSrc);
-    }
-  });
-};
+//     if (!index) {
+//       toggleActiveImage(imageContainer, imageSrc);
+//     }
+//   });
+// };
 
-function copyCodeHandler(event) {
-  const copyButton = event.currentTarget.getElementsByTagName('p')[0];
-  const currentCodeTabId = document.querySelector('[selected-tab]').id;
-  const currentCodeToCopy = EXAMPLE_CODE_TABS[currentCodeTabId];
+// function copyCodeHandler(event) {
+//   const copyButton = event.currentTarget.getElementsByTagName('p')[0];
+//   const currentCodeTabId = document.querySelector('[selected-tab]').id;
+//   const currentCodeToCopy = EXAMPLE_CODE_TABS[currentCodeTabId];
 
-  navigator.clipboard.writeText(currentCodeToCopy.innerText);
+//   navigator.clipboard.writeText(currentCodeToCopy.innerText);
 
-  copyButton.innerHTML = 'Copied';
+//   copyButton.innerHTML = 'Copied';
 
-  setTimeout(() => {
-    copyButton.innerHTML = 'Copy';
-  }, 500);
-}
+//   setTimeout(() => {
+//     copyButton.innerHTML = 'Copy';
+//   }, 500);
+// }
 
-function showAccordionContent(event) {
-  const contentId = event.target.getAttribute('data-accordion');
-  const content = document.querySelector(
-    `[data-accordion-content="${contentId}"]`,
-  );
+// function showAccordionContent(event) {
+//   const contentId = event.target.getAttribute('data-accordion');
+//   const content = document.querySelector(
+//     `[data-accordion-content="${contentId}"]`,
+//   );
 
-  content.style.display = !content?.offsetWidth ? 'block' : 'none';
-}
+//   content.style.display = !content?.offsetWidth ? 'block' : 'none';
+// }
 
-crop.addEventListener('change', onChangeTabsHandler);
-finetune.addEventListener('change', onChangeTabsHandler);
-filter.addEventListener('change', onChangeTabsHandler);
-watermark.addEventListener('change', onChangeTabsHandler);
-annotate.addEventListener('change', onChangeTabsHandler);
-resize.addEventListener('change', onChangeTabsHandler);
-addImg.addEventListener('change', uploadImg);
-modeOptions.addEventListener('change', changeModeHandler);
-jsTabTitle.addEventListener('click', toggleActiveCodeTab);
-reactTabTitle.addEventListener('click', toggleActiveCodeTab);
-cdnTabTitle.addEventListener('click', toggleActiveCodeTab);
-copyButtons.forEach((copyButton) => {
-  copyButton.addEventListener('click', copyCodeHandler);
-});
-accordions.forEach((accordion) => {
-  accordion.addEventListener('click', showAccordionContent);
-});
+// crop.addEventListener('change', onChangeTabsHandler);
+// finetune.addEventListener('change', onChangeTabsHandler);
+// filter.addEventListener('change', onChangeTabsHandler);
+// watermark.addEventListener('change', onChangeTabsHandler);
+// annotate.addEventListener('change', onChangeTabsHandler);
+// resize.addEventListener('change', onChangeTabsHandler);
+// addImg.addEventListener('change', uploadImg);
+// modeOptions.addEventListener('change', changeModeHandler);
+// jsTabTitle.addEventListener('click', toggleActiveCodeTab);
+// reactTabTitle.addEventListener('click', toggleActiveCodeTab);
+// cdnTabTitle.addEventListener('click', toggleActiveCodeTab);
+// copyButtons.forEach((copyButton) => {
+//   copyButton.addEventListener('click', copyCodeHandler);
+// });
+// accordions.forEach((accordion) => {
+//   accordion.addEventListener('click', showAccordionContent);
+// });
